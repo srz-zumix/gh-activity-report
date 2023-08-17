021493add53cefe72fd84cf567cee3b0ef4ca9e6
srz_zumix <zumix.cpp@gmail.com>
2023-08-17 11:17:06 +0900

    no options and tests (#22)

---
+++ .github/scripts/opt-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd -P)
+
+PARAMS=$("${SCRIPT_DIR}/opt-json.sh")
+IFS=$'\n'; for key in $(echo "${PARAMS}" | jq -c -r 'keys | .[]' ); do
+    OPT_PARAMS=$(echo "${PARAMS}" | jq -c ".\"${key}\" | .[]")
+    echo "${key}"
+    OPT_PARAMS_ARRAY=("${OPT_PARAMS}")
+    for opt in "${OPT_PARAMS_ARRAY[@]}"; do
+        echo "  ${opt}"
+        echo "${opt}" | xargs -I {} gh activity-report "${key}" {} > /dev/null
+        # API rate limite workaround
+        sleep "${TEST_SLEEP:-3}"
+    done
+done
