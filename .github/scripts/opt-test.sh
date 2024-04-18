49ea1484d2732a15c69fe3625c106a40a7e838a8
srz_zumix <zumix.cpp@gmail.com>
2024-04-18 23:13:35 +0900

    up

--- .github/scripts/opt-test.sh
+++ .github/scripts/opt-test.sh
@@ -7,6 +7,10 @@ PARAMS=$("${SCRIPT_DIR}/opt-json.sh")
 IFS=$'\n'; for key in $(echo "${PARAMS}" | jq -c -r 'keys | .[]' ); do
     OPT_PARAMS=$(echo "${PARAMS}" | jq -c ".\"${key}\" | .[]")
     echo "${key}"
+    if [ -f "${SCRIPT_DIR}/${key}.done" ]; then
+        echo "  Already tested."
+        continue
+    fi
     OPT_PARAMS_ARRAY=("${OPT_PARAMS}")
     for opt in "${OPT_PARAMS_ARRAY[@]}"; do
         echo "  ${opt}"
@@ -14,4 +18,5 @@ IFS=$'\n'; for key in $(echo "${PARAMS}" | jq -c -r 'keys | .[]' ); do
         # API rate limite workaround
         sleep "${TEST_SLEEP:-3}"
     done
+    touch "${SCRIPT_DIR}/${key}.done"
 done
